c973c47d801b28f5d4c613f461efc327b8ef555f
emanueldias01 <emanueldias01dev@gmail.com>
2025-02-10 18:17:00 -0300

    captura info

--- Bank/src/Main.java
+++ Bank/src/Main.java
@@ -8,6 +8,23 @@ public class Main {
         String agencia;
         String nome;
         double saldo;
+
+    System.out.println("Por favor, insira o numero da conta:");
+    numero = scanner.nextInt();
+
+    //consome a quebra de linha
+    scanner.nextLine();
+
+    System.out.println("Por favor, insira o numero da agencia:");
+    agencia = scanner.nextLine();
+    System.out.println("Por favor, insira seu nome:");
+    nome = scanner.nextLine();
+    System.out.println("Por favor, insira o saldo:");
+    saldo = scanner.nextDouble();
+
+
+    System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agencia e %s, conta %d e seu saldo %.2f ja esta disponivel para o saque", nome, agencia, numero, saldo);
+
     }
 
 }
