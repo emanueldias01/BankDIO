6c2cab131b617ab29eb1f7a1ba05f900f6f0c7ec
emanueldias01 <emanueldias01dev@gmail.com>
2025-02-10 18:06:03 -0300

    criacao dos atributos

---
+++ Bank/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nome;
+        double saldo;
+    }
+
+}
